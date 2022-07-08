01e147da8496e00f0525bf4c142f4186a969c7a0
David Whipple <dwhipp88@gmail.com>
2022-07-07 21:16:29 -0600

    added email validation to form

---
+++ form-validator/script.js
@@ -0,0 +1,59 @@
+const form = document.querySelector('#form');
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const confirmPW = document.querySelector('#confirmPW');
+
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+// Validate Email
+function isValidEmail(email) {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+}
+
+// Event listeners
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (username.value === '') {
+    showError(username, 'Username is required');
+  } else {
+    showSuccess(username);
+  }
+
+  if (email.value === '') {
+    showError(email, 'Email is required');
+  } else if (!isValidEmail(email.value)) {
+    showError(email, 'Invalid email');
+  } else {
+    showSuccess(email);
+  }
+
+  if (password.value === '') {
+    showError(password, 'Password is required');
+  } else {
+    showSuccess(password);
+  }
+
+  if (confirmPW.value === '') {
+    showError(confirmPW, 'Confirm your password');
+  } else {
+    showSuccess(confirmPW);
+  }
+});
